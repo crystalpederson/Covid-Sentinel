77d84763ca549445ffeffef3f63162beaa321537
Michael Chan <mckchan13@gmail.com>
2021-12-31 16:57:14 -0800

    configured basic tailwind, added postcss and tailwind config files, configured webpack and added additional dependencies including

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import Homepage from './Homepage';
 
 const App = () => {
-  return <h1>Hello there</h1>;
+  return (
+  <><h1>Hello there</h1><Homepage /></>
+  )
 };
 
 export default App;
