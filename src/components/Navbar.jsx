971c072bdba5e307c96641fff54f185ca57bcb15
AlexGurfinkel <49167573+AlexGurfinkel@users.noreply.github.com>
2022-01-04 14:58:35 -0800

    map, countries, & about components created

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import Map from './Map';
+import Countries from './Countries';
+import About from './About';
 
 function NavBar () {
   return (
     <div className="container mx-auto">
-      <h1 className="text-blue-900 font-bold">This is the NavBar</h1>
+      <Map />
+      <Countries />
+      <About />
     </div>
   );
 }
