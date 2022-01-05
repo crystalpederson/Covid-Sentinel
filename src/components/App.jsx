7ba938b397d13e7bcf0ed7718cedd9c56a013030
TX Ho <tingxianho@gmail.com>
2022-01-05 10:38:07 +0800

    Setting up footer

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import Homepage from './Homepage';
+import Footer from './Footer';
 
 const App = () => {
-  return (
-  <><h1>Hello there</h1><Homepage /></>
-  )
+	return (
+		<>
+			<h1>Hello there</h1>
+			<Homepage />
+			<Footer />
+		</>
+	);
 };
 
 export default App;
