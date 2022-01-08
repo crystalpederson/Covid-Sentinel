7e9b8358f8cd7c840e8dc75118c3600c56df25dc
Michael Chan <mckchan13@gmail.com>
2022-01-08 11:58:31 -0800

    add about aboutus founders and refactor

---
+++ src/components/AboutUs.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function AboutUs() {
+  return (
+    <div>
+      <h1 className='text-3xl font-bold my-10'>
+			About Us
+      </h1>
+      <div className='md:flex bg-100 my-10'>
+        <h3>Covid Sentinel is a specialized Covid-19 Infection and Vaccination Rate visualization tool, designed to provide guidance for cautious travelers ready to embark on a Covid-19 safe journey. </h3>
+      </div>
+      <div className='md:flex bg-100 my-10'>
+        <h3>Insert Data Source Information</h3>
+      </div>
+    </div>
+  );
+}
+
+export default AboutUs;
---
+++ src/components/Founders.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const Founders = () => {
+  return (
+    <div>
+
+      <h1 className='text-3xl font-bold'>Founders</h1>
+
+      <div className='flex px-5 relative'>
+        <a href='https://www.linkedin.com/in/andresgutierrezramirez/' className='px-5 text-center'>
+          <img src='https://media-exp1.licdn.com/dms/image/C4D03AQFTjqL4RvfVWw/profile-displayphoto-shrink_400_400/0/1612039527571?e=1646870400&v=beta&t=GsrPFuF04VrPPBxltZXvonCjvKkwCv9A4casvsrSpkc' className='bg-red w-32 h-32 rounded-full' />
+          <h1 className='px-5'> Andres </h1>
+        </a>
+			
+        <a href='https://www.linkedin.com/in/michael-chan-77062310/' className='px-5 text-center'>
+          <img src='https://media-exp1.licdn.com/dms/image/C5603AQF4BcjmDb2xtA/profile-displayphoto-shrink_400_400/0/1641161858659?e=1646870400&v=beta&t=yhCpd7rdMIja6vgjHRVo53XjzLQFDpbjH3j1SZB4qE4' className='bg-red w-32 h-32 rounded-full' />
+          <h1 className='px-5'> Michael </h1>
+        </a>
+			
+        <a href='https://www.linkedin.com/in/sa201034/' className='px-5 text-center'>
+          <img src='https://media-exp1.licdn.com/dms/image/C4E03AQEYQkvG2Jv4Bw/profile-displayphoto-shrink_400_400/0/1623884278166?e=1646870400&v=beta&t=lAHl0AOUXRRbEYzva6DaQodzqhGG4jeywZg-_Yp8GYw' className='bg-red w-32 h-32 rounded-full' />
+          <h1 className='px-5'> TX </h1>
+        </a>
+
+        <a href='https://www.linkedin.com/in/vincent-c-084b58190/' className='px-5 text-center'>
+          <img src='https://media-exp1.licdn.com/dms/image/C4D03AQEB9moXc86ooQ/profile-displayphoto-shrink_200_200/0/1570219787681?e=1646870400&v=beta&t=SRtTKeP2QVwdEAWTSjMgc6ayGeYl4J6sYiG0hXwegZw' className='bg-red w-32 h-32 rounded-full' />
+          <h1 className='px-5'> Vince </h1>
+        </a>
+
+        <a href='https://www.linkedin.com/in/alexandergurfinkel/' className='px-5 text-center'>
+          <img src='https://media-exp1.licdn.com/dms/image/C4E03AQE-M6pgM1lDIw/profile-displayphoto-shrink_400_400/0/1517747860761?e=1646870400&v=beta&t=WwYQmSKZi_Yry2TU1HA8zyfPWLumEgfq39JJYT1WDdY' className='bg-red w-32 h-32 rounded-full' />
+          <h1 className='px-5'> Alexander </h1>
+        </a>
+      </div>
+		
+    </div>
+  );
+};
+
+export default Founders;
