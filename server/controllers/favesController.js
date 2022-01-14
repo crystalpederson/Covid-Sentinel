e2784658a9bf6ea57f0abb88c7678427e3f818c1
Crystal Pederson <crystalpederson88@gmail.com>
2022-01-13 19:17:28 -0800

    added middleware for adding countries to faves list

--- server/controllers/favesController.js
+++ server/controllers/favesController.js
@@ -2,6 +2,7 @@ const db = require('../models/userModel');
 
 const favesController = {};
 
+//when given a user id, returns the names of countries that they have favorited
 favesController.getFaves = async (req, res, next) => {
   const id = req.params.id;
   const param = [id];
@@ -17,6 +18,30 @@ favesController.getFaves = async (req, res, next) => {
       log: `Error in favesController.getFaves: ${err}`,
       message: {
         err: 'Error in the backend from favesController.getFaves'
+      }
+    });
+  }
+    
+};
+
+//when given a user id and a country, adds the country to the user's favorites list
+favesController.addFaves = async (req, res, next) => {
+  const { country_name } = req.body;
+  const id = req.params.id;
+  const params = [id, country_name];
+
+  const query = 'INSERT INTO favorites (country_name, user_id) VALUES ($2, $1)';
+  try{
+    const fave = await db.query(query, params);
+
+    res.locals.addedFave = country_name;
+    return next();
+  }
+  catch(err){
+    return next({
+      log: `Error in favesController.addFaves: ${err}`,
+      message: {
+        err: 'Error in the backend from favesController.addFaves'
       }
     });
   }
--- server/routes/faves.js
+++ server/routes/faves.js
@@ -2,10 +2,14 @@ const express = require('express');
 const favesController = require('../controllers/favesController');
 
 const router = express.Router();
-const { getFaves } = favesController;
+const { getFaves, addFaves } = favesController;
 
 router.get('/:id', getFaves, (req, res) => {
   return res.status(200).json(res.locals.favesList);
+});
+
+router.post('/:id', addFaves, (req, res) => {
+  return res.status(200).json(res.locals.addedFave);
 });
 
 module.exports = router;
