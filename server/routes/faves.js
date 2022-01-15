55cf26edfa9a681bf098b585135e6085100f15f3
Jane Park <park.jane.sun@gmail.com>
2022-01-15 10:41:28 -0800

    DELETE router and middleware for favesController.js and faves.js

--- server/routes/faves.js
+++ server/routes/faves.js
@@ -2,7 +2,7 @@ const express = require('express');
 const favesController = require('../controllers/favesController');
 
 const router = express.Router();
-const { getFaves, addFaves } = favesController;
+const { getFaves, addFaves, deleteFaves } = favesController;
 
 router.get('/:id', getFaves, (req, res) => {
   return res.status(200).json(res.locals.favesList);
@@ -10,6 +10,10 @@ router.get('/:id', getFaves, (req, res) => {
 
 router.post('/:id', addFaves, (req, res) => {
   return res.status(200).json(res.locals.addedFave);
+});
+
+router.delete('/:id', deleteFaves, (req, res) => {
+  return res.status(200).json(res.locals.favesList);
 });
 
 module.exports = router;
