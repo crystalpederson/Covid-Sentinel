676c95ae8a05623b906650a3905ac23fe9d5703e
Jane Park <park.jane.sun@gmail.com>
2022-01-19 21:30:13 -0800

    changes

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,17 +1,16 @@
 const express = require('express');
 const userController = require('../controllers/userController');
+const cookieController = require('../controllers/cookieController');
+const sessionController = require('../controllers/sessionController');
 
 const router = express.Router();
-const { signup, login } = userController;
+const { getAllUsers, signup, login } = userController;
+const { isLoggedIn, startSession } = sessionController;
+const { setSSIDCookie } = cookieController; 
 
-router.post('/login', login, (req, res)=>{
-  return res.status(200).json({isMatch: res.locals.isMatch});
-}); 
-
-router.get('/users',
-  userController.getAllUsers,
-  (req, res) => res.status(200).json(res.locals.users)
-);
+router.get('/users', getAllUsers, (req, res) => {
+  res.status(200).json(res.locals.users); 
+});
 
 router.post('/signup', signup, (req, res) => {
   // steps: signup middleare to add user data to database, session middleware, cookie middleware to set a cookie
@@ -19,8 +18,8 @@ router.post('/signup', signup, (req, res) => {
   console.log(req.body);
   res.status(200).send('Account successfully created');
 });
-  
-router.post('/login', login, (req, res) => {
+
+router.post('/login', login, setSSIDCookie, startSession, (req, res) => {
   //router.post('/login', login, setSSIDCookie, (req, res) => {
   // need to add set cookie middleware after login middleware
   // steps: login middleware to verify user data in db, session middleware, cookie middleware to set cookie
@@ -28,6 +27,4 @@ router.post('/login', login, (req, res) => {
   // res.status(200).send('Successfully logged in');
   return res.status(200).json({ isMatch: res.locals.isMatch });
 });
-
-
 module.exports = router;
