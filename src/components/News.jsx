b5857021f6637c06f3ab4cf0ba68013da29241b5
Jessica Wachtel <jlwachtel@gmail.com>
2022-01-20 20:38:12 -0500

    adding

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -9,6 +9,27 @@ import axios from 'axios';
   // can i search through the site to see if that data has a specific country name?
 const News = () => {
 
+ 
+  useEffect (() => {
+
+    console.log('hi')
+
+//     const options = {
+//         method: 'GET',
+//         url: 'https://vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com/api/news/get-coronavirus-news/0',
+//         headers: {
+//           'x-rapidapi-host': 'vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com',
+//           'x-rapidapi-key': '57ba5305b2msh94c110650576e2ap18eccajsnc1c80118e268'
+//         }
+//       };
+    
+//   axios.request(options).then(function (response) {
+//     console.log(response.data);
+//   }).catch(function (error) {
+//     console.error(error);
+//   });
+
+});
 
 return (
     <div>
