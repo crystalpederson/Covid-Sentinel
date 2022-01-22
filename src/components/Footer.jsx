90dba94f37cbb0fb7e7059cb1fdc472ff32133d7
Crystal Pederson <crystalpederson88@gmail.com>
2022-01-22 12:25:26 -0800

    fixed signup styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,13 +7,13 @@ const Footer = () => {
   return (
     <div className="flex justify-center bg-[#EBEBEB] border-t-2">
       <div className="text-left">
-        Copyright &#169; 2022. All rights reserved.
+        Copyright &#169; 2022.  All rights reserved.  Powered by Team Ninja Turtles.
       </div>
       {/* <FontAwesomeIcon
         icon={faGithub}
         className="object-cover w-10 h-10 mx-auto"
       ></FontAwesomeIcon> */}
-      <div className="align-bottom text-right">Powered by XCodeStack.</div>
+      {/* <div className="align-bottom text-right"></div> */}
     </div>
   );
 };
